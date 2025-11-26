c33c1410af3d11c64b910cddc34bab28866b7863
Maks <maks.chojniak@gmail.com>
2025-11-26 15:05:34 +0100

    add repositories implementation for memory in program.cs

--- TravelAgencySystem.UI.Console/Program.cs
+++ TravelAgencySystem.UI.Console/Program.cs
@@ -1,10 +1,19 @@
-﻿using TravelAgencySystem.DataAccess.Abstractions;
+﻿using TravelAgencySystem.DataAccess;
+using TravelAgencySystem.DataAccess.Abstractions;
 using TravelAgencySystem.Database.Abstractions;
 using TravelAgencySystem.Database.Memory;
+using TravelAgencySystem.DataModel;
 
 
-IDbContext db = new MemoryDbContext(); 
+MemoryDbContext db = new MemoryDbContext(); 
 
-IRepository<>
+IRepository<Accomodation> accomodationRepository = new AccomodationRepositoryMemory(db);
+IRepository<AuthCredential> authCredentialRepository = new AuthCredentialRepositoryMemory(db);
+IRepository<Carrier> carrierRepository = new CarrierRepositoryMemory(db);
+IRepository<Client> clientRepository = new ClientRepositoryMemory(db);
+IRepository<Employee> employeeRepository = new EmployeeRepositoryMemory(db);
+IRepository<Offert> offertRepository = new OffertRepositoryMemory(db);
+IRepository<Reservation> reservationRepository = new ReservationRepositoryMemory(db);
+IRepository<Room> roomRepository = new RoomRepositoryMemory(db);
 
 Console.WriteLine("Hello World 2");
