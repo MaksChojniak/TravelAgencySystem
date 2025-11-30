7172dc2720704f11e6bd64f755ffe0a40fe626b6
Maks <maks.chojniak@gmail.com>
2025-11-30 17:55:04 +0100

    add GetHostedOfferts for emplyee

--- TravelAgencySystem.Services/EmployeeService.cs
+++ TravelAgencySystem.Services/EmployeeService.cs
@@ -7,16 +7,20 @@ namespace TravelAgencySystem.Services.Abstractions;
 public class EmployeeService : IEmployeeService
 {
     readonly IRepository<Employee> _employees;
+    readonly IRepository<Offert> _offerts;
 
     readonly IDbContext _dbContext;
 
-    public EmployeeService(IRepository<Employee> employees, IDbContext dbContext)
+    public EmployeeService(IRepository<Employee> employees, IRepository<Offert> offerts, IDbContext dbContext)
     {
         _employees = employees;
+        _offerts = offerts;
         _dbContext = dbContext;
     }
 
     public Employee? Get(Guid id) => _employees.Get(id);
+
+    public IReadOnlyList<Offert> GetHostedOfferts(Guid employeeId) => _offerts.Query().Where(o => o.HostEmployeeId == employeeId).ToList();
 
     public void Update(Guid id, string? firstName, string? lastName, string? pesel, double? salary)
     {
